baa33065e6179fed977526cc5f28fbe64bc29d59
Eden.Kellner <ednkell@gmail.com>
2023-03-04 16:23:04 +0200

    Inversion Function

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,11 @@ impl Vector3 {
     pub fn new(x: Real, y: Real, z: Real) -> Vector3 {
         Vector3 {x, y, z}
     }
+    pub fn invert(&mut self) {
+        self.x = -self.x;
+        self.y = -self.y;
+        self.z = -self.z;
+    }
 }
 
 fn main() {
