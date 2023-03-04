a5cde9c019b0a7ffb4c62b7d4e8cb86e685ade9b
Eden.Kellner <ednkell@gmail.com>
2023-03-04 17:26:25 +0200

    Tests for basic vec3 operations

--- src/vec3.rs
+++ src/vec3.rs
@@ -34,3 +34,47 @@ impl MulAssign<Real> for Vector3 {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn invert_regular() {
+        let mut v = Vector3::new(1.0, -1.5, 1.111);
+        v.invert();
+        assert_eq!(v.x, -1.0);
+        assert_eq!(v.y, 1.5);
+        assert_eq!(v.z, -1.111);
+    }
+
+    #[test]
+    fn invert_zero() {
+        let mut v = Vector3::new(0.0, 0.0, 0.0);
+        v.invert();
+        assert_eq!(v.x, 0.0);
+        assert_eq!(v.y, 0.0);
+        assert_eq!(v.z, 0.0);
+    }
+
+    #[test]
+    fn magnitude() {
+        let v = Vector3::new(1.2, 1.25, 1.4);
+        let res = 2.2276669409945464286979267648430872869963948352545716000891;
+        let acceptable_error = 0.0001;
+
+        assert!(v.magnitude() - res < acceptable_error);
+    }
+
+    #[test]
+    fn normalize_already_normalized() {
+        let f : Real = 3.0;
+        let x = 1.0 / f.sqrt();
+        let mut v = Vector3::new(x, x, x);
+        let acceptable_error = 0.0001;
+        v.normalize();
+
+        assert!(v.x - x < acceptable_error);
+        assert!(v.y - x < acceptable_error);
+        assert!(v.z - x < acceptable_error);
+    }
+}
