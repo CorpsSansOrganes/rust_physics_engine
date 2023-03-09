e3d14e107b7f05331eaa2ac2501057d7b61aa399
Eden.Kellner <ednkell@gmail.com>
2023-03-09 09:55:31 +0200

    dot product, cross product

--- src/vec3.rs
+++ src/vec3.rs
@@ -4,7 +4,8 @@ use std::ops::AddAssign; // += overloading
 use std::ops::Add; // + overloading                   
 use std::ops::SubAssign; // -= overloading
 use std::ops::Sub; // - overloading
-
+use std::ops::DivAssign; // /= overloading
+use std::ops::Div; // / overloading
 type Real = f32; // Accuracy vs. Percision tradeoff.
 
 pub struct Vector3 {
@@ -29,14 +30,34 @@ impl Vector3 {
         // Maybe should handle magnitude < 1?
         *self *= 1.0 / self.magnitude();
     }
+    pub fn dot_product(&self, rhs: &Vector3) -> Real {
+        /*
+         * Dot product, aka inner product and scalar product.
+         */
+        self.x * rhs.x + self.y * rhs.y + self.z * rhs.z
+    }
+    pub fn cross_product(&self, rhs: &Vector3) -> Vector3 {
+        let x = self.y * rhs.z - self.z * rhs.y;
+        let y = -self.x * rhs.z + self.z * rhs.x;
+        let z = self.x * rhs.y - self.y * rhs.x;
+        Vector3::new(x, y, z)
+    }
+}
+
+impl Copy for Vector3 { }
+
+impl Clone for Vector3 {
+    fn clone(&self) -> Vector3 {
+        *self
+    }
 }
 
 impl MulAssign<Real> for Vector3 {
     fn mul_assign(&mut self, rhs: Real) {
-       self.x *= rhs; 
-       self.y *= rhs; 
-       self.z *= rhs; 
-       // *self = *self * rhs; // todo()!
+       // self.x *= rhs; 
+       // self.y *= rhs; 
+       // self.z *= rhs; 
+       *self = *self * rhs; 
     }
 }
 
@@ -50,11 +71,28 @@ impl Mul<Real> for Vector3 {
     }    
 }
 
+impl DivAssign<Real> for Vector3 {
+    fn div_assign(&mut self, rhs: Real) {
+        *self = *self / rhs
+    }
+}
+
+impl Div<Real> for Vector3 {
+    type Output = Vector3;
+    fn div(self, rhs: Real) -> Self::Output {
+       let x = self.x / rhs;
+       let y = self.y / rhs;
+       let z = self.z / rhs;
+       Vector3::new(x, y, z)
+    }
+}
+
 impl AddAssign for Vector3 {
    fn add_assign(&mut self, rhs: Self) {
-       self.x += rhs.x; 
-       self.y += rhs.y; 
-       self.z += rhs.z; 
+       // self.x += rhs.x; 
+       // self.y += rhs.y; 
+       // self.z += rhs.z; 
+       *self = *self + rhs;
    } 
 }
 impl Add for Vector3 {
@@ -69,9 +107,10 @@ impl Add for Vector3 {
 
 impl SubAssign for Vector3 {
    fn sub_assign(&mut self, rhs: Self) {
-       self.x -= rhs.x; 
-       self.y -= rhs.y; 
-       self.z -= rhs.z; 
+       // self.x -= rhs.x; 
+       // self.y -= rhs.y; 
+       // self.z -= rhs.z; 
+       *self = *self - rhs;
    } 
 }
 impl Sub for Vector3 {
@@ -143,5 +182,64 @@ mod tests {
         assert_eq!(res.x, 3.0);
         assert_eq!(res.y, 1.0);
         assert_eq!(res.z, 3.0);
+
+        res /= 2.0;
+        assert_eq!(res.x, 1.5);
+        assert_eq!(res.y, 0.5);
+        assert_eq!(res.z, 1.5);
+
+        res *= -1.0;
+        assert_eq!(res.x, -1.5);
+        assert_eq!(res.y, -0.5);
+        assert_eq!(res.z, -1.5);
+    }
+
+    #[test]
+    fn dot_product_test() {
+        let v1 = Vector3::new(1.0, 0.0, 0.0);
+        let v2 = Vector3::new(0.0, 1.0, 0.0);
+
+        // v1 & v2 are perpendicular to each other, therefore their dot 
+        // product should be 0.
+        assert_eq!(v1.dot_product(&v2), 0.0);
+
+        let v1 = Vector3::new(1.0, 1.0, 0.0);
+        let v2 = Vector3::new(3.0, 2.3, 0.0);
+        // v1 & v2 point in the same direction, therefore their dot product 
+        // should be positive.
+        assert!(v1.dot_product(&v2) > 0.0);
+        assert_eq!(v1.dot_product(&v2), 5.3);
+
+        let v1 = Vector3::new(1.0, 1.0, 0.0);
+        let v2 = Vector3::new(-3.0, -2.3, 3.0);
+        // v1 & v2 point in opposite directions, therefore their dot product 
+        // should be negative.
+        assert!(v1.dot_product(&v2) < 0.0);
+        assert_eq!(v1.dot_product(&v2), -5.3);
+    }
+
+    #[test]
+    fn cross_product_test() {
+        let v1 = Vector3::new(2.0, 0.0, 0.0);
+        let v2 = Vector3::new(0.0, 2.0, 0.0);
+
+        // The area of the parallelogram formed by v1 & v2 is 4, which should
+        // be the magnitude of their cross product.
+        assert_eq!(v1.cross_product(&v2).magnitude(), 4.0);
+
+        let v1 = Vector3::new(-1.0, 3.2, 4.0);
+        let v2 = Vector3::new(-2.2, 1.0, -0.5);
+        let res = v1.cross_product(&v2);
+
+        // The cross product should be perpendicular to both v1 and v2. 
+        // Therefore, the dot product should be 0 for both v1 and v2.
+        let acceptable_error = 0.00001;
+        assert!(v1.dot_product(&res) < acceptable_error);
+        assert!(v2.dot_product(&res) < acceptable_error);
+
+        // Assert we got the expected result (and not the left-hand vector)
+        assert!(res.x - -5.6 < acceptable_error);
+        assert!(res.y - -9.3 < acceptable_error);
+        assert!(res.z - 6.04 < acceptable_error);
     }
 }
